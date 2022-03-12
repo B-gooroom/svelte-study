e5173037b4bbf3859a970f2a26fae806b022a162
B-gooroom <j.sean1316@gmail.com>
2022-03-12 16:52:20 +0900

    router link

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,6 +1,12 @@
+<script>
+  import { link } from "svelte-spa-router";
+  import active from "svelte-spa-router/active";
+</script>
+
 <nav class="nav">
   <ul>
-    <li><h2>Members</h2></li>
-    <li><h2>Search</h2></li>
+    <li><h2><a href="/" use:link use:active>Home</a></h2></li>
+    <li><h2><a href="/members" use:link use:active>Members</a></h2></li>
+    <li><h2><a href="/search" use:link use:active>Search</a></h2></li>
   </ul>
 </nav>
